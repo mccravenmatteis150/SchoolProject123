278a7eef650961c7f1c8be70f709e8b4e9f9f6f2
mccravenmatteis150 <mccravenmatteis150@gmail.com>
2025-03-07 10:20:40 +0000

    Add 3.cpp

---
+++ src/3.cpp
@@ -0,0 +1,8 @@
+#include <iostream>
+using namespace std;
+int main() {
+    int age = 18;
+    string name = "Alice";
+    cout << "Hello, my name is " << name << " and I am " << age << " years old." << endl;
+    return 0;
+}
