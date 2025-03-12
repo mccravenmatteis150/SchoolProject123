d78ef33d715c5faf74260e2bb60ddc0a814a900f
mccravenmatteis150 <mccravenmatteis150@gmail.com>
2025-03-12 03:01:38 +0000

    Add 8.cpp

---
+++ src/8.cpp
@@ -0,0 +1,10 @@
+
+int main() {
+    int x = 5;
+    if (x > 0) {
+        cout << "The value of x is positive." << endl;
+    } else {
+        cout << "The value of x is negative or zero." << endl;
+    }
+    return 0;
+}
