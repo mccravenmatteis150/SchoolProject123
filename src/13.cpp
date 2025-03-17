66fd3008dca1cc6858e648755fe6c31506cdc191
mccravenmatteis150 <mccravenmatteis150@gmail.com>
2025-03-17 07:33:15 +0000

    Add 13.cpp

---
+++ src/13.cpp
@@ -0,0 +1,19 @@
+int main() {
+    // Declare variables
+    int x = 5;
+    int y = 10;
+    
+    // Perform arithmetic operations
+    int sum = x + y;
+    int difference = x - y;
+    int product = x * y;
+    int quotient = x / y;
+    
+    // Print results
+    std::cout << "Sum: " << sum << std::endl;
+    std::cout << "Difference: " << difference << std::endl;
+    std::cout << "Product: " << product << std::endl;
+    std::cout << "Quotient: " << quotient << std::endl;
+    
+    return 0;
+}
