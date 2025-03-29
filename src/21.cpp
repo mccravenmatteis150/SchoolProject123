03d721df9ecf0adf35d55000862dad6491ac5de3
mccravenmatteis150 <mccravenmatteis150@gmail.com>
2025-03-29 20:11:52 +0000

    Add 21.cpp

---
+++ src/21.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int x = 5;
+    int y = 10;
+    
+    cout << "x + y = " << (x + y) << endl; // Output: x + y = 15
+    
+    return 0;
+}
