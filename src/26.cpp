b88aa940e25e74ffb54e9a61274f0bb101976a45
mccravenmatteis150 <mccravenmatteis150@gmail.com>
2025-04-05 05:12:33 +0000

    Add 26.cpp

---
+++ src/26.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int num1 = 5;
+    int num2 = 3;
+    
+    if (num1 > num2) {
+        cout << "The first number is greater than the second." << endl;
+    } else {
+        cout << "The first number is not greater than the second." << endl;
+    }
+    
+    return 0;
+}
