78ac57e4ca10e67323ff42dcaf51155000936770
mccravenmatteis150 <mccravenmatteis150@gmail.com>
2025-05-24 10:07:51 +0000

    Add 41.cpp

---
+++ src/41.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int x = 5;
+    double y = 3.14;
+    
+    cout << "Value of x is: " << x << endl;
+    cout << "Value of y is: " << y << endl;
+    
+    return 0;
+}
